2220b7b939016bf07a6106441dfba26957776c13
dododorpm <108949699+dododorpm@users.noreply.github.com>
2026-06-01 22:01:20 +0200

    Refactor resizing functions and improve click handling
    changement du button par un wait_next_event

--- page_accueil.ml
+++ page_accueil.ml
@@ -1,95 +1,92 @@
 open Graphics
+
 let continue = ref true
 
-let l = int_of_string(read_line())
-let h= int_of_string(read_line())
+(* Lecture résolution utilisateur *)
+let l = float_of_int (int_of_string (read_line ()))
+let h = float_of_int (int_of_string (read_line ()))
 
-let resizey y =
-   int_of_float((float_of_int(y)/.500.)*. float_of_int(h))
+(* Scale functions (base 700x500) *)
+let sx x =
+  int_of_float ((float_of_int x /. 700.) *. l)
 
-let resizex x =
-   int_of_float((float_of_int(x)/.700.)*. float_of_int(l))
+let sy y =
+  int_of_float ((float_of_int y /. 500.) *. h)
 
+(* Taille des cases en base *)
+let cell_w = 100
+let cell_h = 100
 
-let affichniveau niveau x y =
+(* Dessine une case complète *)
+let draw_case bx by n =
+  let x = sx bx in
+  let y = sy by in
+  let w = sx (bx + cell_w) - sx bx in
+  let h = sy (by + cell_h) - sy by in
+
+  (* fond *)
+  set_color green;
+  fill_rect x y w h;
+
+  (* bord *)
+  set_color black;
+  draw_rect x y w h;
+
+  (* texte *)
   set_color red;
-  set_text_size 5;
-  moveto ((resizex x) + (resizex 47)) ((resizey y) + (resizey 20));
-  draw_string (string_of_int(niveau));
-  moveto ((resizex x) + (resizex 23)) ((resizey y) + (resizey 60));
-  draw_string ("Niveau")
+  moveto (x + w / 3) (y + h / 3);
+  draw_string (string_of_int n)
 
+(* test clic dans une case *)
+let inside x y bx by =
+  let x0 = sx bx in
+  let y0 = sy by in
+  let w = sx (bx + cell_w) - sx bx in
+  let h = sy (by + cell_h) - sy by in
 
+  x >= x0 && x <= x0 + w &&
+  y >= y0 && y <= y0 + h
 
+(* dessin global *)
 let dessin () =
   set_color black;
-  (* titre snake *)
-  moveto (resizex 326) (resizey 400);
-  set_text_size 1;
+
+  moveto (sx 300) (sy 420);
   draw_string "SNAKE";
 
-  (* Cases et niveaux *)
-  draw_rect (resizex 100) (resizey 250) (resizex 100) (resizey 100);
-  draw_rect (resizex 300) (resizey 250) (resizex 100) (resizey 100);
-  draw_rect (resizex 500) (resizey 250) (resizex 100) (resizey 100);
-  draw_rect (resizex 200) (resizey 100) (resizex 100) (resizey 100);
-  draw_rect (resizex 400) (resizey 100) (resizex 100) (resizey 100);
-  set_color green;
-  fill_rect (resizex 101) (resizey 251) (resizex 98) (resizey 98);
-  fill_rect (resizex 301) (resizey 251) (resizex 98) (resizey 98);
-  fill_rect (resizex 501) (resizey 251) (resizex 98) (resizey 98);
-  fill_rect (resizex 201) (resizey 101) (resizex 98) (resizey 98);
-  fill_rect (resizex 401) (resizey 101) (resizex 98) (resizey 98);
-  affichniveau 1 (100) (250);
-  affichniveau 2 (300) (250);
-  affichniveau 3 (500) (250);
-  affichniveau 4 (200) (100);
-  affichniveau 5 (400) (100)
+  draw_case 100 250 1;
+  draw_case 300 250 2;
+  draw_case 500 250 3;
+  draw_case 200 100 4;
+  draw_case 400 100 5
 
-
-
-
+(* gestion clic *)
 let whereclick () =
-  if button_down () then
-    let (x,y) = mouse_pos () in
-    if x >= resizex 100 && x <= resizex 200 && y >= resizey 250 && y <= resizey 350 then
-      begin
-        continue := false;
-        print_int(1)
-      end;
-    if x >= resizex 300 && x <= resizex 400 && y >= resizey 250 && y <= resizey 350 then
-      begin
-        continue := false;
-        print_int(2)
-      end;
-    if x >= resizex 500 && x <= resizex 600 && y >= resizey 250 && y <= resizey 350 then
-      begin
-        continue := false;
-        print_int(3)
-      end;
-    if x >= resizex 200 && x <= resizex 300 && y  >= resizey 100 && y <= resizey 200 then begin
-        continue := false;
-        print_int(4)
-      end;
-    if x >= resizex 400 && x <= resizex 500 && y >= resizey 100 && y <= resizey 200 then
-      begin
-        continue := false;
-        print_int(5)
-      end
+  let e = wait_next_event [Button_down] in
+  let x = e.mouse_x in
+  let y = e.mouse_y in
 
+  if inside x y 100 250 then (print_int 1; continue := false)
+  else if inside x y 300 250 then (print_int 2; continue := false)
+  else if inside x y 500 250 then (print_int 3; continue := false)
+  else if inside x y 200 100 then (print_int 4; continue := false)
+  else if inside x y 400 100 then (print_int 5; continue := false)
+
+(* gestion clavier *)
 let test () =
   if key_pressed () then
     match read_key () with
     | 'c' -> continue := false
     | _ -> ()
 
-let str = " " ^ string_of_int(l) ^ "x" ^ string_of_int(h)
-
+(* main *)
 let () =
-  open_graph str;
-  dessin();
+  open_graph (" " ^ string_of_int (int_of_float l) ^ "x" ^ string_of_int (int_of_float h));
+  dessin ();
+
   while !continue do
     test ();
     whereclick ()
   done;
-  close_graph ();
+
+  close_graph ()
